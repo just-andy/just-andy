2b4258a20e0bcec8707cf8f866c2a994624aa23d
Andys Macbook Pro <andy@just-andy.uk>
2020-07-07 00:01:07 +0100

    base starting point

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
+/** @jsx jsx */
 import React from "react"
+import { Styled, jsx } from "theme-ui"
+import Layout from "../components/Layout"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = () => {
+  return <Layout>
+    
+    <Styled.h1>Hello world!</Styled.h1>
+    <Styled.p>Welcome to your new Gatsby site.</Styled.p>
+    <Styled.p>Now go build something great.</Styled.p>
+    </Layout>
 }
+
+export default Home
